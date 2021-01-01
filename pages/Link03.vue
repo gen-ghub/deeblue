4c168efa0a342829d4a85262aa2ef3cd90e6482e
gen-ghub <renbenzhuang234@gmail.com>
2021-01-01 10:28:33 +0900

    お問い合わせエラー対策

--- pages/Link03.vue
+++ pages/Link03.vue
@@ -1,30 +1,30 @@
 <template>
-  <div class="contact">
+  <div class="contact border-b-2 ">
     <div class="flex justify-center">
       <form class="p-contact__form text-2xl " name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" @submit.prevent="onSubmit" :class="sendingClass">
         <input type="hidden" name="form-name" value="contact">
         <div class="p-contact__item flex flex-col">
-          <label class="form-label pb-2" for="username">お名前</label>
+          <label class="form-label pb-2 text-white" for="username">お名前</label>
           <input class="contact-input rounded" type="text" id="username" name="username" v-model="username" autocomplete="name">
         </div>
         <div class="p-contact__item flex flex-col">
-          <label class="form-label pb-2" for="katakana">フリガナ</label>
+          <label class="form-label pb-2 text-white" for="katakana">フリガナ</label>
           <input class="rounded" type="text" id="katakana" name="katakana" v-model="katakana">
         </div>
         <div class="p-contact__item flex flex-col">
-          <label class="form-label pb-2" for="useremail">メールアドレス</label>
+          <label class="form-label pb-2 text-white" for="useremail">メールアドレス</label>
           <input class="rounded" type="text" id="useremail" name="useremail" v-model="useremail" autocomplete="email">
         </div>
         <div class="p-contact__item flex flex-col">
-          <label class="form-label pb-2" for="message">お問い合わせ内容</label>
-          <textarea class="rounded" id="message" name="message" v-model="message"></textarea>
+          <label class="form-label pb-2 text-white" for="message">お問い合わせ内容</label>
+          <textarea class="rounded h-40" id="message" name="message" v-model="message"></textarea>
         </div>
         <div class="p-contact__item" v-show="false">
           <label for="message">スパムでない場合は空欄</label>
           <input type="text" name="bot-field" v-model="botField"/>
         </div>
-        <div class="p-contact__submit flex justify-center">
-          <button class="rounded" type="submit">送信</button>
+        <div class="p-contact__submit flex justify-center mt-8">
+          <button class="rounded text-white border border-light-blue-500 border-opacity-75 py-2 px-20 hover:bg-blue-900 " type="submit">送信</button>
         </div>
       </form>
     </div>
@@ -42,7 +42,6 @@
   padding-top: 20vh;
   height: 92vh;
   background-color:#022444 ;
-  color: #fff;
 }
 .contact-input{
   width: 20vw;
