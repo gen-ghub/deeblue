514232796914cf6282165b8e0567aa80ef514bef
gen-ghub <renbenzhuang234@gmail.com>
2021-01-08 20:41:57 +0900

    テキスト内容記述

--- pages/Link01.vue
+++ pages/Link01.vue
@@ -8,12 +8,17 @@
         <div class="col-start-2 col-span-10 mt-56">
           <div class="flex">
             <div class="title ">
-              <h1 class="text-5xl font-black leading-relaxed">事業内容のタイトル</h1>
+              <h1 class="text-5xl font-black leading-relaxed">”体験”としてのサービス</h1>
             </div>
             <div class="title-txt ">
-              <h1 class="text-4xl font-black leading-relaxed">多忙の原因を特定する<br>
-              「学校の健康診断」</h1>
-              <p class="mt-16">「学校の健康診断」を通して、多忙の原因を明らかにし、先生の働き方を変え、より子どもに向き合える学校づくりを目指しています。全国に先駆けて取り組む柏市で成功事例をつくり、全国へと拡げています。</p>
+              <h1 class="text-4xl font-black leading-relaxed">これからの時代は<br>
+              何を体験してくかが鍵</h1>
+              <div class="mt-16">
+                <p class="mb-1">これまでより更に急激に発達するこれからの社会</p>
+                <p class="mb-1">AIの発達によりモノもより溢れ、製造自動化により価格も下がり物への価値は下がっていく。</p>
+                <p class="mb-1">人も労働時間もより減っていき、週3日労働の将来すら現実味を帯びてきている。</p>
+                <p class="mb-1">そんな時代に要求されるモノは”体験”へのサービスだと考えている。</p>
+              </div>
             </div>
           </div>
         </div>
@@ -27,8 +32,45 @@
         <div class="business-bg">
           <div class="mt-32 w-1/2 ml-32 ">
             <div class="py-20 text-white">
-              <h1 class="pb-10 pt-2 mt-32 text-3xl font-bold">BBQ設備レンタル</h1>
-              <p class="text-sm w-3/4 mt-8 leading-relaxed">柏の葉小学校では、過労死ラインを超過する先生の割合を昨年同月比43%から0%まで削減し、専門性の向上に時間を投資できている先生の割合を28%から60%まで増加させるなどの成果に繋げました。柏の葉小学校における先生の出勤から退勤までの行動観察や、学校に届く書類1500部の仕分けなど、現場に深く入り込んだ調査を経て、「学校の健康診断」は開発されました。今後は、全国にこのモデルを広げると共に、効果的な治療の開発に取り組んでいきます。</p>
+              <h1 class="pb-10 mt-24 text-4xl font-bold">「BBQ出張レンタル」</h1>
+              <p class="text-sm w-3/4 mt-8 leading-loose tracking-widest">関東・関西の2拠点で幅広いエリアでBBQ用品を中心としたアウトドア用品の出張レンタルサービス。
+              お客様の希望する場所・時間・内容で要望に応じた機材や食材をお届けしています。
+              友達、会社、家族、様々なお客様に対応し、手ぶらでBBQを楽しんで頂けるサービスを提供しています。</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="mt-40 grid grid-cols-12 relative">
+      <div class="absolute top-0 left-0 ">
+        <img class="image-size" src="~/assets/biru.jpg" />
+      </div>
+      <div class="col-start-6 col-span-8 ">
+        <div class="business-bg">
+          <div class="mt-32 pl-32 pt-32">
+            <div class="text-white pl-48 pt-20">
+              <h1 class="pl-12 text-4xl font-bold">「生ビールサーバー</h1>
+              <h1 class="pl-32 pb-10 text-4xl font-bold">出張レンタルサービス」</h1>
+              <p class="pl-20 text-sm w-3/4 pt-8 leading-loose">会社、レンタルスペース、自宅にキャンプ場などどこでもキンキンに冷えた樽生ビールをお届けするサービス。
+町のお祭りや懇親会、クリスマスや忘年会などでご利用頂けます。
+</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="mt-40 grid grid-cols-12 relative">
+      <div class="absolute top-0 right-0 ">
+        <img class="image-size" src="~/assets/camp.jpg" />
+      </div>
+      <div class="col-start-1 col-span-8 ">
+        <div class="business-bg">
+          <div class="mt-32 w-1/2 ml-32 ">
+            <div class="py-20 text-white">
+              <h1 class="pb-10 pt-2 mt-32 text-3xl font-bold">「公園バーベキュー管理事業」</h1>
+              <p class="text-sm w-3/4 mt-8 leading-loose">滋賀県大津市、琵琶湖のほとりの大津市なぎさ公園の運営管理を大津市公園緑地協会より指定委託運営業者として行っております。
+バーベキュー＆アウトドア用品レンタルに、SUPなどウォーターアクティビティーとしてのレンタルも行っております。
+今後はキャンプ場としての運営も企画中。</p>
             </div>
           </div>
         </div>
@@ -41,39 +83,12 @@
       <div class="col-start-6 col-span-8 ">
         <div class="business-bg">
           <div class="mt-32 pl-32 pt-32">
-            <div class="text-white pl-48 pt-20">
-              <h1 class="pl-20 pb-10 text-3xl font-bold">ブライダル</h1>
-              <p class="pl-20 text-sm w-3/4 pt-8 leading-relaxed">柏の葉小学校では、過労死ラインを超過する先生の割合を昨年同月比43%から0%まで削減し、専門性の向上に時間を投資できている先生の割合を28%から60%まで増加させるなどの成果に繋げました。柏の葉小学校における先生の出勤から退勤までの行動観察や、学校に届く書類1500部の仕分けなど、現場に深く入り込んだ調査を経て、「学校の健康診断」は開発されました。今後は、全国にこのモデルを広げると共に、効果的な治療の開発に取り組んでいきます。</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    <section class="mt-40 grid grid-cols-12 relative">
-      <div class="absolute top-0 right-0 ">
-        <img class="image-size" src="~/assets/biru.jpg" />
-      </div>
-      <div class="col-start-1 col-span-8 ">
-        <div class="business-bg">
-          <div class="mt-32 w-1/2 ml-32 ">
-            <div class="py-20 text-white">
-              <h1 class="pb-10 pt-2 mt-32 text-3xl font-bold">ビールサーバレンタル</h1>
-              <p class="text-sm w-3/4 mt-8 leading-relaxed">柏の葉小学校では、過労死ラインを超過する先生の割合を昨年同月比43%から0%まで削減し、専門性の向上に時間を投資できている先生の割合を28%から60%まで増加させるなどの成果に繋げました。柏の葉小学校における先生の出勤から退勤までの行動観察や、学校に届く書類1500部の仕分けなど、現場に深く入り込んだ調査を経て、「学校の健康診断」は開発されました。今後は、全国にこのモデルを広げると共に、効果的な治療の開発に取り組んでいきます。</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    <section class="mt-40 grid grid-cols-12 relative">
-      <div class="absolute top-0 left-0 ">
-        <img class="image-size" src="~/assets/camp.jpg" />
-      </div>
-      <div class="col-start-6 col-span-8 ">
-        <div class="business-bg">
-          <div class="mt-32 pl-32 pt-32">
-            <div class="text-white pl-48 pt-20">
-              <h1 class="pl-20 pb-10 text-3xl font-bold">キャンプ場管理</h1>
-              <p class="pl-20 text-sm w-3/4 pt-8 leading-relaxed">柏の葉小学校では、過労死ラインを超過する先生の割合を昨年同月比43%から0%まで削減し、専門性の向上に時間を投資できている先生の割合を28%から60%まで増加させるなどの成果に繋げました。柏の葉小学校における先生の出勤から退勤までの行動観察や、学校に届く書類1500部の仕分けなど、現場に深く入り込んだ調査を経て、「学校の健康診断」は開発されました。今後は、全国にこのモデルを広げると共に、効果的な治療の開発に取り組んでいきます。</p>
+            <div class="text-white pl-48 pt-16">
+              <h1 class="pl-12 text-4xl font-bold">「フラッシュモブ</h1>
+              <h1 class="pl-32 pb-10 text-4xl font-bold">ダンス企画演出・撮影代行」</h1>
+              <p class="pl-20 text-sm w-3/4 pt-8 leading-loose">結婚式をより盛り上げるサービス。
+余興や演出企画代行としてダンスを生かしてフラッシュモブなど、様々な演出で結婚式や披露宴、二次会を盛り上げるお手伝いを行います。
+撮影クルーによる企業の商品プロモーション撮影代行も行っております。</p>
             </div>
           </div>
         </div>
