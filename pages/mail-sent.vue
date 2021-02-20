726ac008f1a6706ee090f5aa93326edb62ea2b0c
gen-ghub <renbenzhuang234@gmail.com>
2021-02-20 12:39:05 +0900

    修正

--- pages/mail-sent.vue
+++ pages/mail-sent.vue
@@ -1,10 +1,19 @@
 <template>
-  <section class="">
-
+  <section class="p-40">
+    <h1 class="flex justify-center m-40">
+      お問い合わせ頂きありがとうございました。
+    </h1>
   </section>
 </template>
 
 
 <style lang="scss" scoped>
-
+section{
+  height: 100vh;
+  background-color: #022444;
+  color: #fff;
+  h1{
+    font-size: 30px;
+  }
+}
 </style>
